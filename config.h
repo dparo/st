5da1aedd5ce0c2ed815ae11e90f93336932f825e
Davide Paro <paro.dav.95@gmail.com>
2020-05-09 16:36:29 +0200

    Fixed font stuff.

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Cascadia Code:pixelsize=14:antialias=true:autohint=true";
-static char *font2[] = { "mono:pixelsize=14:antialias=true:autohint=true" };
+static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
